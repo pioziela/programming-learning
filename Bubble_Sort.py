d32fa12907d5e209724dd1176b9a161e7c5b4cef
zielapio <piotr.zielinski.cv@gmail.com>
2020-05-09 11:36:08 +0200

    fix coun swaps return value

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -2,15 +2,18 @@ with open('bubble_sort_table.txt', "r") as text:
     text_in_file = text.read()
     table_numbers = list(map(int, text_in_file.rstrip().split()))
 
-def Count_Swaps(list):
+
+def count_Swaps(list):
     change = lambda list, index2: list.__setitem__(slice(index2, index2 + 2), (list[index2 + 1], list[index2]))
     swaps_list = [change(list, index2) for index, element in enumerate(list) for index2, element2 in enumerate(list) if index2 < len(list) - 1 and list[index2] > list[index2 + 1]]
-    First_Element = list[0]
-    Last_Element = list[len(list)-1]
-    Number_of_Swaps = len(swaps_list)
-    print ('Array is sorted in',Number_of_Swaps, 'swaps.', end='\n')
-    print('First Element:',First_Element)
-    print('Last Element:', Last_Element)
+    first_element = list[0]
+    last_element = list[len(list)-1]
+    number_of_swaps = len(swaps_list)
+    return number_of_swaps, first_element, last_element
 
 
-Count_Swaps(table_numbers)
+swaps_number, first_el, last_el = count_Swaps(table_numbers)
+
+print('Array is sorted in', swaps_number, 'swaps.', end='\n')
+print('First Element:', first_el)
+print('Last Element:', last_el)
