e7aded5586d2f557e76e470ffe889bedb36424e8
zielapio <piotr.zielinski.cv@gmail.com>
2020-05-19 12:43:54 +0200

    Delete function changing position, with open in function, correction of docstring

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,29 +1,36 @@
-"""pobranie z pliku listy nieposortowanych liczb:"""
-with open('bubble_sort_table.txt', "r") as text:
-    text_in_file = text.read()
-    table_numbers = list(map(int, text_in_file.rstrip().split()))
-
-
-def change_element_position(elements, position):
-    """Funkcja zmieniająca pozycję dwóch elementów w liście."""
-    elements[position], elements[position + 1] = elements[position + 1], elements[position]
+def getting_integers():
+    """
+    The function getting_integers downloading from the file list of integers to sort.
+    """
+    with open('bubble_sort_table.txt', "r") as text:
+        text_in_file = text.read()
+        table_numbers = list(map(int, text_in_file.rstrip().split()))
+        return table_numbers
 
 
 def count_swaps(numbers_to_sort):
-    """Funkcja sortująca liczby na liście metodą sortowania bąbelkowego."""
-    number_of_swaps = 0  # licznik ilości wykonanych zmian pozycji elementów w liście
+    """
+    The function count_swaps sorting numbers in the list by bubble sorting.
+    """
+    number_of_swaps = 0
     for index_1 in range(len(numbers_to_sort)):
         for index_2 in range(len(numbers_to_sort)-1):
             if numbers_to_sort[index_2] > numbers_to_sort[index_2+1]:
-                change_element_position(numbers_to_sort, index_2)
+                numbers_to_sort[index_2], numbers_to_sort[index_2 + 1] =\
+                    numbers_to_sort[index_2 + 1], numbers_to_sort[index_2]
                 number_of_swaps += 1
-    first_element = numbers_to_sort[0]  # pierwszy element w posortowanej liście liczb
-    last_element = numbers_to_sort[len(numbers_to_sort) - 1]  # ostatni element w posortowanej liście liczb
+    first_element = numbers_to_sort[0]
+    last_element = numbers_to_sort[len(numbers_to_sort) - 1]
     return number_of_swaps, first_element, last_element
 
 
-swaps_number, first_el, last_el = count_swaps(table_numbers)
+swaps_number, first_el, last_el = count_swaps(getting_integers())
 
-print('Array is sorted in', swaps_number, 'swaps.', end='\n')
-print('First Element:', first_el)
-print('Last Element:', last_el)
+
+def printing_results():
+    print('Array is sorted in', swaps_number, 'swaps.', end='\n')
+    print('First Element:', first_el)
+    print('Last Element:', last_el)
+
+
+printing_results()
