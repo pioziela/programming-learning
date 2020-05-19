4df4b5ab3e0196fbe641b5ec5afc54314bdc38ed
zielapio <piotr.zielinski.cv@gmail.com>
2020-05-19 11:22:12 +0200

    Ransome Note correct - with open closed in function and English docstrings

--- Hash_Tables_Ransome_Note.py
+++ Hash_Tables_Ransome_Note.py
@@ -1,18 +1,27 @@
-"""
-pobranie pliku w którym pierwsza linia zawiera dwie liczby, które określają ilość
-słów w tekście z czasopisma (w drugiej linni) i w trzeciej linii ilość słów repliki oryginalnego tekstu.
-"""
-with open('text_for_hash_tables.txt', "r") as text:
-    for line_number in range(0, 3):
-        line = text.readline()
-        if line_number == 1:
-            words_in_second_line = list(line.rstrip().split())
-        elif line_number == 2:
-            words_in_third_line = list(line.rstrip().split())
+def getting_text():
+    """
+    The function getting_text downloads a file in which the first line contains two numbers
+    that specify the number of words in the magazine text (in the second line)
+    and in the third line the number of words of the replica of the original text.
+    """
+    with open('text_for_hash_tables.txt', "r") as text:
+        line_number = 0
+        for line in text:
+            if line_number == 0:
+                pass
+            elif line_number == 1:
+                words_in_second_line = list(line.rstrip().split())
+            elif line_number == 2:
+                words_in_third_line = list(line.rstrip().split())
+            line_number += 1
+    return words_in_second_line, words_in_third_line
 
 
 def compare_text(oryginal_words, replica_words):
-    """funkcja sprawdzająca czy z tekstu oryginalnego można stworzyć jego replikę"""
+    """
+    The function compare_text checks if a replica
+    can be created from the original text.
+    """
     for words in replica_words:
         count_of_word = replica_words.count(words)
         for words_2 in oryginal_words:
@@ -25,5 +34,6 @@ def compare_text(oryginal_words, replica_words):
     return 'Yes'
 
 
-yes_or_no = compare_text(words_in_second_line, words_in_third_line)
+oryginal_text, replica_text = getting_text()
+yes_or_no = compare_text(oryginal_text, replica_text)
 print(yes_or_no)
