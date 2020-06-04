378476679765b8bb2f3b35d278f72c48daf5eb5a
zielapio <piotr.zielinski.cv@gmail.com>
2020-06-04 14:28:20 +0200

    shorter while loop, printing exceptions in the same function

--- Hacker_Rank_Exceptions.py
+++ Hacker_Rank_Exceptions.py
@@ -10,28 +10,17 @@ def divide_exceptions():
     The function returns a division result or a corresponding error.
     """
     line_number = 0
-    number_of_lines = 0
-    error_messages = []
-    while line_number <= number_of_lines:
-            line = sys.stdin.readline()
-            if line_number == 0:
-                number_of_lines = int(line)
-            elif 0 < line_number <= number_of_lines:
-                numbers_to_divide = list(map(str, line.rstrip().split()))
-                try:
-                    error_messages.append(int(int(numbers_to_divide[0]) / int(numbers_to_divide[1])))
-                except ZeroDivisionError:
-                    error_messages.append("Error Code: integer division or modulo by zero")
-                except ValueError as invalid_literal:
-                    error = "Error Code:" + ' ' + str(invalid_literal)
-                    error_messages.append(error)
-            line_number += 1
-    return error_messages
+    number_of_lines = int(sys.stdin.readline())
+    while line_number < number_of_lines:
+        numbers_to_divide = list(map(str, sys.stdin.readline().rstrip().split()))
+        try:
+            print(int(numbers_to_divide[0]) / int(numbers_to_divide[1]))
+        except ZeroDivisionError:
+            print("Error Code: integer division or modulo by zero")
+        except ValueError as invalid_literal:
+            error = f"Error Code: {invalid_literal}"
+            print(error)
+        line_number += 1
 
 
-def printing_exceptions(exceptions_list):
-    for index in exceptions_list:
-        print(index)
-
-
-printing_exceptions(divide_exceptions())
+divide_exceptions()
