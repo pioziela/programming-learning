31e785a6836115d9208b40b7ece4d7a5f3ee868f
zielapio <piotr.zielinski.cv@gmail.com>
2020-06-17 10:02:39 +0200

    unknown max and min

--- Counting_Sort.py
+++ Counting_Sort.py
@@ -1,14 +1,25 @@
-input_numbers_to_sort = [1, 4, 1, 8, 9, 2, -1, 3, 7, 5, 2, 133, 0, 8, 8, 8]
+input_numbers_to_sort = [1, 4, 1, 8, 9, 101, 2, -1, 3, 7, 5, 2, 13, 0, 8, 8, 8]
 
 
 def counting_sort(numbers_to_sort):
-    max_element = max(numbers_to_sort)
-    min_element = min(numbers_to_sort)
-    elements_and_numbers_of_elements_count = {}
-    counter_of_elements = 0
+    multiplier = 1
+    whether_done = False
+    while whether_done is False:
+        max_element = 10 * multiplier
+        min_element = -10 * multiplier
+        elements_and_numbers_of_elements_count = {}
+        counter_of_elements = 0
+        for element in numbers_to_sort:
+            if element < min_element or element > max_element:
+                whether_done = False
+                multiplier = multiplier * 10
+                break
+            else:
+                whether_done = True
     for i in range(min_element, max_element+1):
         counter_of_elements = counter_of_elements + numbers_to_sort.count(i)
         elements_and_numbers_of_elements_count[i] = counter_of_elements
+        print(elements_and_numbers_of_elements_count)
     output_sort_numbers = []
     counter_decrease = 0
     for key in elements_and_numbers_of_elements_count:
